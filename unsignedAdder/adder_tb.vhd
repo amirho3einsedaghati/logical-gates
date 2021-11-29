b85a761d054b8c16faa95cc6e3a5d761b14e0563
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-11-29 14:17:42 +0330

    edit some comments from adder_tb.vhd

--- unsignedAdder/adder_tb.vhd
+++ unsignedAdder/adder_tb.vhd
@@ -1,38 +1,7 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   01:11:51 11/28/2021
--- Design Name:   
--- Module Name:   E:/Research/terme 5 - shahre qods/digital system design/Xilinx Project/adder/adder_tb.vhd
--- Project Name:  adder
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: adder
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use IEEE.numeric_std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
 ENTITY adder_tb IS
 END adder_tb;
  
@@ -55,8 +24,6 @@ ARCHITECTURE behavior OF adder_tb IS
 
  	--Outputs
    signal sum : unsigned(4 downto 0);
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
  
  
 BEGIN
